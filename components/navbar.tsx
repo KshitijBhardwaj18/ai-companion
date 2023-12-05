bc1f0718a7dbfc96dbfb3456b4db3b56e96f4a77
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2023-12-05 15:06:34 +0530

    Completed sidebar and partial search box

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,30 +1,44 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Menu, Sparkles } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
 
-const font = Poppins({weight: "600",subsets:["latin"]})
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
 
-export const Navbar = ()  => {
-    return(
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b  border-primary/10 bg-secondary "> 
-            <div className="flex items-center">
-                <Menu className="block md:hidden"/>
-                <Link href="/">
-                    <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
-                    font.className)}>
-                        companion.ai
-                    </h1>
-                </Link>
-            </div>
+const font = Poppins({ weight: "600", subsets: ["latin"] });
 
-            <div className="flex items-center gap-x-3">
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b  border-primary/10 bg-secondary h-16 ">
+      <div className="flex items-center">
+        <MobileSidebar />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            companion.ai
+          </h1>
+        </Link>
+      </div>
 
-                <UserButton/>
-            </div>
-        </div>
-    )
-}
+      <div className="flex items-center gap-x-3">
+        <Button size="sm" variant="premium">
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white" />
+        </Button>
+
+        <ModeToggle />
+
+        <UserButton />
+      </div>
+    </div>
+  );
+};
