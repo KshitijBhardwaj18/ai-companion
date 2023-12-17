024a869217d08d7cc3aeb2dffc43fc8317d4bbc5
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2023-12-17 20:01:50 +0530

    Made Ui and Api for chat

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -1,15 +1,19 @@
-import prismadb from "@/lib/prismadb";
-import { auth, redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
 import { ChatClient } from "./components/client";
 
 interface ChatIdPageProps {
   params: {
     chatId: string;
-  };
+  }
 }
 
-const ChatIdPage = async ({ params }: ChatIdPageProps) => {
+const ChatIdPage = async ({
+  params
+}: ChatIdPageProps) => {
   const { userId } = auth();
 
   if (!userId) {
@@ -18,34 +22,33 @@ const ChatIdPage = async ({ params }: ChatIdPageProps) => {
 
   const companion = await prismadb.companion.findUnique({
     where: {
-      id: params.chatId,
+      id: params.chatId
     },
     include: {
       messages: {
         orderBy: {
-          createdAt: "asc",
+          createdAt: "asc"
         },
         where: {
-            userId
-        }
-
+          userId,
+        },
       },
-
       _count: {
         select: {
-            messages: true
+          messages: true,
         }
       }
-    },
+    }
   });
 
+
   if (!companion) {
-    return redirect("/")
+    return redirect("/");
   }
 
   return (
-    <ChatClient companion={companion}/>
+    <ChatClient companion={companion} />
   );
-};
-
+}
+ 
 export default ChatIdPage;
--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -2,7 +2,7 @@
 
 import { Companion } from "@prisma/client";
 
-import { useState,useEffect } from "react";
+import { useState,useEffect, useRef } from "react";
 
 import { ChatMessage, ChatMessageProps } from "@/components/chat-message";
 
@@ -17,21 +17,49 @@ export const ChatMessages = ({
   isLoading,
   companion,
 }: ChatMessagesProps) => {
-    const [fakeLoading, setFakeLoading] = useState(message.length === 0 ? true : false);
+    const scrollRef = useRef<ElementRef<"div">>(null);
+
+    const [fakeLoading, setFakeLoading] = useState(messages.length === 0 ? true : false);
 
     useEffect(() => {
         const timeout = setTimeout(() => {
             setFakeLoading(false);
-        }, 100)
+        }, 1000)
+
+        return () => {
+          clearTimeout(timeout);
+        }
+    }, [messages.length]);
+
+    useEffect(() => {
+      scrollRef?.current?.scrollIntoView({ behavior: "smooth"});
     }, [])
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
-        
+        isLoading={fakeLoading}
         src={companion.src}
         role="system"
         content={`Hello, I am ${companion.name}, ${companion.description}`}
       />
+
+      {
+        messages.map((message) => (
+          <ChatMessage
+            key={message.content}
+            role={message.role}
+            content={message.content}
+            src={message.src}
+          />
+
+        )) 
+      }
+
+      {isLoading && (
+        <ChatMessage role="system" src={companion.src} isLoading/>
+      )}
+
+      <div ref={scrollRef} />
 
       
 
